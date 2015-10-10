1127e01bc1c0c971031a8b244f912a72cfb5f8cf
takepong <takepong@mac.com>
2015-10-11 03:37:31 +0900

    マウスカーソルの座標をワールド座標に変換するサンプルScene00、Scene01を追加。

---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorController : MonoBehaviour {
+	public GameObject cursorPrefab;
+	GameObject cursor;
+	public Camera mainCamera;
+	// Use this for initialization
+	void Start () {
+		cursor = Instantiate(cursorPrefab) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mouse = Input.mousePosition;			// マウスカーソルの位置を取ってきて
+		Ray ray = mainCamera.ScreenPointToRay(mouse);	// カメラ位置からのＲａｙに変換する
+		RaycastHit hit;									// Ｒａｙのヒット情報の入れ物を用意して
+		if(Physics.Raycast(ray,out hit)){				// ヒットしてたら
+			Vector3 pos = hit.point;					// 衝突位置と
+			Vector3 norm = hit.normal;					// 衝突した面の法線をもらってきて
+			cursor.transform.position = pos;			// カーソル用のオブジェクトの位置と方向を合わせる
+			cursor.transform.rotation = Quaternion.FromToRotation(Vector3.up,norm);
+			cursor.SetActive(true);						// 
+		}else{
+			cursor.SetActive(false);					// ヒットしなかったら表示しない
+		}
+	}
+}
---
+++ Assets/Scripts/CursorController2D.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorController2D : MonoBehaviour {
+	public Camera mainCamera;
+	GameObject dragTarget;
+	float targetZ;
+	Vector3 targetOffset;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mouse = Input.mousePosition;				// マウスカーソルの位置を取ってきて
+		Vector3 pos = mainCamera.ScreenToWorldPoint(mouse);	// カメラ位置からワールド座標に変換する
+		if(Input.GetMouseButtonDown(0)){					// クリックされたら
+			RaycastHit2D hit = Physics2D.Raycast(pos,-Vector2.up,0.01f);	// ヒットチェック
+			if(hit.collider!=null){											// ヒットしてたら
+				targetOffset = hit.collider.transform.position - pos;		// 位置の差分を保存して
+				targetOffset.z = 0;											// z方向は無視する
+				targetZ = hit.collider.transform.position.z;				// ｚ座標を保存して
+				pos.z = targetZ;											// 
+				hit.collider.transform.position = pos + targetOffset;		// マウスのワールド座標に移動
+				dragTarget = hit.collider.gameObject;		// つかんだOBJは保持しておく
+			}else{
+				dragTarget = null;	//つかんでない
+			}
+		}else if(Input.GetMouseButton(0)){					// ボタンが押された状態なら（ドラッグ中なら）
+			if(dragTarget!=null){							// つかんだOBJが無ければ何もしない
+				pos.z = targetZ;
+				dragTarget.transform.position = pos + targetOffset;		// マウス座標についてくる
+			}
+		}else{
+			dragTarget = null;
+		}
+	}
+}
